88ee12252589ca8866f294bc89cc76de259ca7c2
1DARKle1 <voercecs11@gmail.com>
2026-06-17 15:31:32 +0300

    base: create git practice project

---
+++ main.py
@@ -0,0 +1,18 @@
+def show_project_title():
+    print("Git practice project started")
+
+
+def show_student_info():
+    student_name = "Фамилия Имя"
+    group = "Название группы"
+    print(f"Student: {student_name}")
+    print(f"Group: {group}")
+
+
+def main():
+    show_project_title()
+    show_student_info()
+
+
+if __name__ == "__main__":
+    main()
